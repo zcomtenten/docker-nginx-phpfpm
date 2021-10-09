8bc4025c2998cf05d9994b5e22818d49e68c9274
Vouu <contact@manhtuong.net>
2021-10-10 00:29:28 +0700

    Create insatll.sh

---
+++ insatll.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+function nginx_port_checker() {
+    while :; do
+        port=$(shuf -i 2000-4000 -n 1)
+        port_checking=$(netstat -nplt | grep $port)
+        port_checkingv2=$(find ./ -type f -name "*.yaml" -exec grep '$port' {} \;)
+        if [ -z "$port_checking" ]; then
+            if [ -z "$port_checkingv2" ]; then
+                break
+            fi
+        fi
+    done
+
+}
+
+function nginx_docker_name_random() {
+    while :; do
+        nginx_creat_name=$(openssl rand -hex 8)
+        nginx_creat_name_check=$(find ./ -type f -name "*.yaml" -exec grep '$db_creat' {} \;)
+        if [ -z "$nginx_creat_name_check" ]; then
+            break
+        fi
+    done
+
+}
+
+function creat_nginx_docker_file() {
+    cp -r core $nginx_creat_name 
+    cat > $nginx_creat_name/docker-compser.yaml <<EOF
+version: '3'
+services:
+  web:
+      build:
+        context: .
+        dockerfile: ./containers/nginx/Dockerfile
+      ports:
+          - "$port:80"
+      volumes:
+        - ./nginx.conf:/etc/nginx/nginx.conf
+        - ./default.conf:/etc/nginx/conf.d/default.conf
+      depends_on:
+          - php
+  php:
+      build:
+        context: .
+        dockerfile: ./containers/php/Dockerfile
+      volumes:
+        - ./code:/var/www/html
+EOF
+
+
+nginx_port_checker
+nginx_docker_name_random
+creat_nginx_docker_file
